e425feb5b7df810c819358a8f66135e696d9fbce
Samuel K. Gutierrez <samuel@lanl.gov>
2021-06-24 12:30:34 -0600

    Add cudart and nvml support.
    Add first cut of mostly configuration that is passed to hwloc.
    
    Signed-off-by: Samuel K. Gutierrez <samuel@lanl.gov>

--- cmake/hwloc.cmake
+++ cmake/hwloc.cmake
@@ -16,6 +16,8 @@ set(QVI_HWLOC_DIR ${CMAKE_CURRENT_SOURCE_DIR}/deps/hwloc-2.5.0.tar.gz)
 set(QVI_HWLOC_BIN ${CMAKE_CURRENT_BINARY_DIR}/hwloc)
 set(QVI_HWLOC_STATIC_LIB ${QVI_HWLOC_BIN}/lib/libhwloc.a)
 set(QVI_HWLOC_INCLUDES ${QVI_HWLOC_BIN}/include)
+# A list containing any relevant GPU flags required in hwloc configuration.
+set(QVI_HWLOC_GPU_FLAGS "")
 
 file(MAKE_DIRECTORY ${QVI_HWLOC_INCLUDES})
 
@@ -28,6 +30,24 @@ else()
     # Ninja doesn't like $(MAKE), so just use make
     set(QVI_HWLOC_BUILD_COMMAND "make")
 endif()
+
+# TODO(skg) Add option to turn this off even if found.
+if(CUDAToolkit_FOUND AND NOT QV_DISABLE_GPU_SUPPORT)
+    list(APPEND QVI_HWLOC_GPU_FLAGS "--enable-cuda=yes")
+    list(APPEND QVI_HWLOC_GPU_FLAGS "--enable-nvml=yes")
+    # TODO(skg) This may be incomplete. Assumes we have pkg-config.
+    list(APPEND QVI_HWLOC_GPU_FLAGS "--with-cuda-version=${CUDAToolkit_VERSION}")
+else()
+    list(APPEND QVI_HWLOC_GPU_FLAGS "--enable-cuda=no")
+    list(APPEND QVI_HWLOC_GPU_FLAGS "--enable-nvml=no")
+endif()
+
+message(STATUS "hwloc GPU configure flags are:")
+list(APPEND CMAKE_MESSAGE_INDENT "  ")
+foreach(flag ${QVI_HWLOC_GPU_FLAGS})
+    message(STATUS "${flag}")
+endforeach()
+list(POP_BACK CMAKE_MESSAGE_INDENT)
 
 ExternalProject_Add(
     libhwloc
@@ -47,10 +67,9 @@ ExternalProject_Add(
       --enable-cairo=no
       --enable-gl=no
       --enable-opencl=no
-      --enable-cuda=no
-      --enable-nvml=no
       --enable-pci=no
       --enable-libudev=no
+      ${QVI_HWLOC_GPU_FLAGS}
     BUILD_COMMAND ${QVI_HWLOC_BUILD_COMMAND}
     INSTALL_COMMAND ${QVI_HWLOC_BUILD_COMMAND} install
     BUILD_BYPRODUCTS ${QVI_HWLOC_STATIC_LIB}
