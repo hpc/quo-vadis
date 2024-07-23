343c77f1a079b020c16c47c53ecd2513d84183b9
Samuel K. Gutierrez <samuel@lanl.gov>
2024-07-23 14:15:02 -0600

    Checkpoint some pthread group work.
    Adding full pthread group support is non-trivial, so we will leave group
    splitting as not supported until we have some cycles to dedicate to this. The
    basic structure is there, but we need nice way to share data across the
    threads during gather, scatter, etc.
    
    Signed-off-by: Samuel K. Gutierrez <samuel@lanl.gov>

--- src/qvi-group.h
+++ src/qvi-group.h
@@ -93,18 +93,7 @@ struct qvi_group_s : qvi_refc_s {
     static int
     next_id(
         qvi_group_id_t *gid
-    ) {
-        // Global group ID. Note that we pad its initial value so that other
-        // infrastructure (e.g., QVI_MPI_GROUP_WORLD) will never equal or exceed
-        // this value.
-        static std::atomic<qvi_group_id_t> group_id(64);
-        if (group_id == UINT64_MAX) {
-            qvi_log_error("Group ID space exhausted");
-            return QV_ERR_OOR;
-        }
-        *gid = group_id++;
-        return QV_SUCCESS;
-    }
+    );
 };
 typedef struct qvi_group_s qvi_group_t;
 
