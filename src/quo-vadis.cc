022a2f16c1260de5253893e4121bfc6001e79a0e
Samuel K. Gutiérrez <samuel@lanl.gov>
2025-05-29 13:20:48 -0600

    Cleanup and simplify more code. (#334)
    Signed-off-by: Samuel K. Gutierrez <samuel@lanl.gov>

--- src/quo-vadis.cc
+++ src/quo-vadis.cc
@@ -16,6 +16,7 @@
 
 // IWYU pragma: begin_keep
 #include "qvi-common.h"
+#include "qvi-group-process.h"
 #include "qvi-scope.h"
 // IWYU pragma: end_keep
 
@@ -34,6 +35,35 @@ qv_version(
     *patch = PACKAGE_VERSION_PATCH;
 
     return QV_SUCCESS;
+}
+
+static int
+qvi_process_scope_get(
+    qv_scope_intrinsic_t iscope,
+    qv_scope_t **scope
+) {
+    // Create the base process group.
+    qvi_group_process *zgroup = nullptr;
+    const int rc = qvi_new(&zgroup);
+    if (qvi_unlikely(rc != QV_SUCCESS)) {
+        *scope = nullptr;
+        return rc;
+    }
+    return qv_scope::make_intrinsic(zgroup, iscope, scope);
+}
+
+int
+qv_process_scope_get(
+    qv_scope_intrinsic_t iscope,
+    qv_scope_t **scope
+) {
+    if (qvi_unlikely(!scope)) {
+        return QV_ERR_INVLD_ARG;
+    }
+    try {
+        return qvi_process_scope_get(iscope, scope);
+    }
+    qvi_catch_and_return();
 }
 
 int
