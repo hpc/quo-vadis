7777411f34f750e2fb8c9429bf1b1d419f0f7b2c
Samuel K. Gutiérrez <samuel@lanl.gov>
2025-08-09 22:36:05 -0600

    Add more tests. (#392)
    Signed-off-by: Samuel K. Gutierrez <samuel@lanl.gov>

--- tests/test-mpi-init.c
+++ tests/test-mpi-init.c
@@ -1,14 +1,4 @@
 /* -*- Mode: C; c-basic-offset:4; indent-tabs-mode:nil -*- */
-/*
- * Copyright (c) 2020-2024 Triad National Security, LLC
- *                         All rights reserved.
- *
- * Copyright (c) 2020-2021 Lawrence Livermore National Security, LLC
- *                         All rights reserved.
- *
- * This file is part of the quo-vadis project. See the LICENSE file at the
- * top-level directory of this distribution.
- */
 
 /**
  * @file test-mpi-init.c
