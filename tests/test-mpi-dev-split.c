079a7e13f981a5f939873e412369c485c30c9b5a
Samuel K. Gutierrez <samuel@lanl.gov>
2026-06-24 11:27:29 -0600

    Generalize MPI device split test.
    Signed-off-by: Samuel K. Gutierrez <samuel@lanl.gov>

--- tests/test-mpi-dev-split.c
+++ tests/test-mpi-dev-split.c
@@ -1,18 +1,4 @@
 /* -*- Mode: C; c-basic-offset:4; indent-tabs-mode:nil -*- */
-/*
- * Copyright (c) 2020-2026 Triad National Security, LLC
- *                         All rights reserved.
- *
- * Copyright (c) 2020-2021 Lawrence Livermore National Security, LLC
- *                         All rights reserved.
- *
- * This file is part of the quo-vadis project. See the LICENSE file at the
- * top-level directory of this distribution.
- */
-
-/**
- * @file test-mpi-phases.c
- */
 
 #include "quo-vadis-mpi.h"
 #include "common-test-utils.h"
@@ -24,8 +10,9 @@ main(
 ) {
     char const *ers = NULL;
     MPI_Comm comm = MPI_COMM_WORLD;
+    qv_hw_obj_type_t target_dev = QV_HW_OBJ_GPU;
+    char const *dev_name = ctu_obj_name(target_dev);
 
-    /* Initialization */
     int rc = MPI_Init(&argc, &argv);
     if (rc != MPI_SUCCESS) {
         ers = "MPI_Init() failed";
@@ -61,82 +48,83 @@ main(
         ctu_panic("%s (rc=%s)", ers, qv_strerr(rc));
     }
 
-    int ngpus;
-    rc = qv_scope_hw_obj_count(base_scope, QV_HW_OBJ_GPU, &ngpus);
+    int ndevs;
+    rc = qv_scope_hw_obj_count(base_scope, target_dev, &ndevs);
     if (rc != QV_SUCCESS) {
       ers = "qv_scope_hw_obj_count() failed";
       ctu_panic("%s (rc=%s)", ers, qv_strerr(rc));
     }
 
-    if (ngpus == 0) {
-        if (wrank == 0) printf("Skipping: no GPUs found!\n");
+    if (ndevs == 0) {
+        if (wrank == 0) printf("Skipping: no %ss found!\n", dev_name);
         goto done;
     }
     else {
-        if (wrank == 0) printf("# Number of GPUs found: %d\n", ngpus);
+        if (wrank == 0) printf("# Number of %ss found: %d\n", dev_name, ndevs);
     }
 
-    qv_scope_t *gpu_scope;
+    qv_scope_t *dev_scope;
     rc = qv_scope_split_at(
         base_scope,
-        QV_HW_OBJ_GPU,
+        target_dev,
         QV_SCOPE_SPLIT_AFFINITY_PRESERVING,
-        &gpu_scope
+        &dev_scope
     );
     if (rc != QV_SUCCESS) {
         ers = "qv_scope_split_at() failed";
         ctu_panic("%s (rc=%s)", ers, qv_strerr(rc));
     }
 
-    int my_gpu_rank;
+    int my_dev_rank;
     rc = qv_scope_group_rank(
-        gpu_scope,
-        &my_gpu_rank
+        dev_scope,
+        &my_dev_rank
     );
     if (rc != QV_SUCCESS) {
         ers = "qv_scope_group_rank() failed";
         ctu_panic("%s (rc=%s)", ers, qv_strerr(rc));
     }
 
-    rc = qv_scope_bind_push(gpu_scope);
+    rc = qv_scope_bind_push(dev_scope);
     if (rc != QV_SUCCESS) {
         ers = "qv_bind_push() failed";
         ctu_panic("%s (rc=%s)", ers, qv_strerr(rc));
     }
 
-    int my_ngpus;
+    int my_ndevs;
     rc = qv_scope_hw_obj_count(
-            gpu_scope,
-            QV_HW_OBJ_GPU,
-            &my_ngpus);
+        dev_scope,
+        target_dev,
+        &my_ndevs
+    );
     if (rc != QV_SUCCESS) {
         ers = "qv_scope_hw_obj_count() failed";
         ctu_panic("%s (rc=%s)", ers, qv_strerr(rc));
     }
 
-    /* Where did I end up? */
+    // Where did I end up?
     char *binds;
-    rc = qv_scope_bind_string(gpu_scope, QV_BIND_STRING_LOGICAL, &binds);
+    rc = qv_scope_bind_string(dev_scope, QV_BIND_STRING_LOGICAL, &binds);
     if (rc != QV_SUCCESS) {
         ers = "qv_bind_get_list_as_string() failed";
         ctu_panic("%s (rc=%s)", ers, qv_strerr(rc));
     }
     printf(
-        "=> [%d] Split@GPU: got %d GPUs, running on %s\n",
-        wrank, my_ngpus, binds
+        "=> [%d] Split@Dev: got %d %s(s), running on %s\n",
+        wrank, my_ndevs, dev_name, binds
     );
     free(binds);
 
-    for (int i = 0; i < my_ngpus; ++i) {
-        char *gpu;
+    for (int i = 0; i < my_ndevs; ++i) {
+        char *devid;
         qv_scope_device_id_get(
-            gpu_scope, QV_HW_OBJ_GPU, i, QV_DEVICE_ID_PCI_BUS_ID, &gpu
+            dev_scope, target_dev, i, QV_DEVICE_ID_PCI_BUS_ID, &devid
         );
-        printf("   [%d] GPU %d PCI Bus ID = %s\n", wrank, i, gpu);
-        free(gpu);
+        printf("   [%d] Dev %d PCI Bus ID = %s\n", wrank, i, devid);
+        free(devid);
     }
 
-    rc = qv_scope_free(gpu_scope);
+    rc = qv_scope_free(dev_scope);
     if (rc != QV_SUCCESS) {
         ers = "qv_scope_free() failed";
         ctu_panic("%s (rc=%s)", ers, qv_strerr(rc));
