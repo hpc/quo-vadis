212dfee216fedd4cde1b5b301441ddbe665274c1
Samuel K. Gutiérrez <samuel@lanl.gov>
2026-08-26 15:06:55 -0600

    Make test-mpi-phases.c a standalone example. (#585)
    Signed-off-by: Samuel K. Gutierrez <samuel@lanl.gov>

--- examples/example-mpi-phases.c
+++ examples/example-mpi-phases.c
@@ -1,12 +1,34 @@
 /* -*- Mode: C; c-basic-offset:4; indent-tabs-mode:nil -*- */
 
+/*
+ * Copyright (c) 2020-2026 Triad National Security, LLC
+ *                         All rights reserved.
+ *
+ * Copyright (c) 2020-2021 Lawrence Livermore National Security, LLC
+ *                         All rights reserved.
+ *
+ * This file is part of the quo-vadis project. See the LICENSE file at the
+ * top-level directory of this distribution.
+ */
+
 /**
- * @file test-mpi-phases.c
+ * @file example-mpi-phases.c
  */
 
 #include "quo-vadis-mpi.h"
-#include "common-test-utils.h"
+
+#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
+
+#define panic(...)                                                             \
+do {                                                                           \
+    fprintf(stderr, "\n%s@%d: ", __func__, __LINE__);                          \
+    fprintf(stderr, __VA_ARGS__);                                              \
+    fprintf(stderr, "\n");                                                     \
+    fflush(stderr);                                                            \
+    exit(EXIT_FAILURE);                                                        \
+} while (0)
 
 static void
 sleep_ms(long ms) {
@@ -27,9 +49,9 @@ do_omp_things(
     int rc = qv_hw_obj_count(scope, QV_HW_OBJ_PU, &npus);
     if (rc != QV_SUCCESS) {
         char const *ers = "qv_hw_obj_count() failed";
-        ctu_panic("%s (rc=%s)", ers, qv_strerr(rc));
+        panic("%s (rc=%s)", ers, qv_strerr(rc));
     }
-    ctu_logf(
+    printf(
         "[%d] %s: doing OpenMP things on %d PUs...\n",
         base_rank, scope_name, npus
     );
@@ -45,9 +67,9 @@ do_pthread_things(
     int rc = qv_hw_obj_count(scope, QV_HW_OBJ_CORE, &ncores);
     if (rc != QV_SUCCESS) {
         char const *ers = "qv_hw_obj_count() failed";
-        ctu_panic("%s (rc=%s)", ers, qv_strerr(rc));
+        panic("%s (rc=%s)", ers, qv_strerr(rc));
     }
-    ctu_logf(
+    printf(
         "[%d] %s: doing Pthread things on %d Cores...\n",
         base_rank, scope_name, ncores
     );
@@ -57,7 +79,7 @@ do_pthread_things(
 static void
 print_cpus(int rank, char *header, qv_scope_t *scope, char *scope_name)
 {
-    if (rank == 0) ctu_logf("\n# %s\n", header);
+    if (rank == 0) printf("\n# %s\n", header);
     // Not needed; used so that header prints first
     sleep_ms(1);
 
@@ -66,9 +88,9 @@ print_cpus(int rank, char *header, qv_scope_t *scope, char *scope_name)
     int rc = qv_bind_string(scope, QV_BIND_STRING_PHYSICAL, &binds);
     if (rc != QV_SUCCESS) {
         ers = "qv_bind_string() failed";
-        ctu_panic("%s (rc=%s)", ers, qv_strerr(rc));
+        panic("%s (rc=%s)", ers, qv_strerr(rc));
     }
-    ctu_logf("[%d] %s: Running on CPUs %s\n",
+    printf("[%d] %s: Running on CPUs %s\n",
              rank, scope_name, binds);
     free(binds);
 }
@@ -77,11 +99,8 @@ print_cpus(int rank, char *header, qv_scope_t *scope, char *scope_name)
 static void
 print_resources(int rank, char *header, qv_scope_t *scope, char *scope_name)
 {
-    ctu_str_t *str = ctu_str_new();
     if (rank == 0)
-        ctu_str_appendf(
-            str, "\n# %s\n", header
-        );
+        printf("\n# %s\n", header);
     else
         // Not needed; used so that header prints first
         sleep_ms(1);
@@ -91,10 +110,10 @@ print_resources(int rank, char *header, qv_scope_t *scope, char *scope_name)
     int rc = qv_bind_string(scope, QV_BIND_STRING_PHYSICAL, &binds);
     if (rc != QV_SUCCESS) {
         ers = "qv_bind_string() failed";
-        ctu_panic("%s (rc=%s)", ers, qv_strerr(rc));
+        panic("%s (rc=%s)", ers, qv_strerr(rc));
     }
-    ctu_str_appendf(
-        str, "[%d] %s: Running on CPUs %s\n", rank, scope_name, binds
+    printf(
+        "[%d] %s: Running on CPUs %s\n", rank, scope_name, binds
     );
     free(binds);
 
@@ -106,7 +125,7 @@ print_resources(int rank, char *header, qv_scope_t *scope, char *scope_name)
     );
     if (rc != QV_SUCCESS) {
         ers = "qv_hw_obj_count() failed";
-        ctu_panic("%s (rc=%s)", ers, qv_strerr(rc));
+        panic("%s (rc=%s)", ers, qv_strerr(rc));
     }
 
     int ncores;
@@ -117,7 +136,7 @@ print_resources(int rank, char *header, qv_scope_t *scope, char *scope_name)
     );
     if (rc != QV_SUCCESS) {
         ers = "qv_hw_obj_count() failed";
-        ctu_panic("%s (rc=%s)", ers, qv_strerr(rc));
+        panic("%s (rc=%s)", ers, qv_strerr(rc));
     }
 
     int ngpus;
@@ -128,35 +147,27 @@ print_resources(int rank, char *header, qv_scope_t *scope, char *scope_name)
     );
     if (rc != QV_SUCCESS) {
         ers = "qv_hw_obj_count() failed";
-        ctu_panic("%s (rc=%s)", ers, qv_strerr(rc));
+        panic("%s (rc=%s)", ers, qv_strerr(rc));
     }
 
-    ctu_str_appendf(
-        str,
+    printf(
         "[%d] %s: Got %d NUMAs %d Cores %d GPUs",
         rank, scope_name, nnumas, ncores, ngpus
     );
 
     if (ngpus > 0) {
-        ctu_str_appendf(
-            str, "\n[%d] %s: GPUs ", rank, scope_name
-        );
+        printf("\n[%d] %s: GPUs ", rank, scope_name);
         char *gpu;
         for (int i = 0; i < ngpus; i++) {
             qv_device_id(
                  scope, QV_HW_OBJ_GPU, i, QV_DEVICE_ID_PCI_BUS_ID, &gpu
             );
-            ctu_str_appendf(
-                str, "%s ", gpu
-            );
+            printf("%s ", gpu);
         }
         free(gpu);
     }
 
-    ctu_logf(
-        "%s\n", ctu_str_cstr(str)
-    );
-    ctu_str_del(str);
+    printf("\n");
 }
 
 // Split at GPUs.
@@ -177,14 +188,14 @@ void split_at_gpu(int rank, qv_scope_t *scope, int color, char *header)
     char const *ers = NULL;
     if (rc != QV_SUCCESS) {
         ers = "qv_split_at() failed";
-        ctu_panic("%s (rc=%s)", ers, qv_strerr(rc));
+        panic("%s (rc=%s)", ers, qv_strerr(rc));
     }
 
     // Move into my scope
     rc = qv_bind_push(gpu_scope);
     if (rc != QV_SUCCESS) {
         ers = "qv_bind_push() failed";
-        ctu_panic("%s (rc=%s)", ers, qv_strerr(rc));
+        panic("%s (rc=%s)", ers, qv_strerr(rc));
     }
 
     print_resources(rank, header, gpu_scope, "gpu_scope");
@@ -196,7 +207,7 @@ void split_at_gpu(int rank, qv_scope_t *scope, int color, char *header)
     );
     if (rc != QV_SUCCESS) {
         ers = "qv_group_rank() failed";
-        ctu_panic("%s (rc=%s)", ers, qv_strerr(rc));
+        panic("%s (rc=%s)", ers, qv_strerr(rc));
     }
 
     int my_ngpus;
@@ -207,19 +218,19 @@ void split_at_gpu(int rank, qv_scope_t *scope, int color, char *header)
     );
     if (rc != QV_SUCCESS) {
         ers = "qv_hw_obj_count() failed";
-        ctu_panic("%s (rc=%s)", ers, qv_strerr(rc));
+        panic("%s (rc=%s)", ers, qv_strerr(rc));
     }
 
     rc = qv_bind_pop(gpu_scope);
     if (rc != QV_SUCCESS) {
         ers = "qv_bind_pop() failed";
-        ctu_panic("%s (rc=%s)", ers, qv_strerr(rc));
+        panic("%s (rc=%s)", ers, qv_strerr(rc));
     }
 
     rc = qv_free(gpu_scope);
     if (rc != QV_SUCCESS) {
         ers = "qv_free() failed";
-        ctu_panic("%s (rc=%s)", ers, qv_strerr(rc));
+        panic("%s (rc=%s)", ers, qv_strerr(rc));
     }
 }
 
@@ -235,7 +246,7 @@ main(
     int rc = MPI_Init(&argc, &argv);
     if (rc != MPI_SUCCESS) {
         ers = "MPI_Init() failed";
-        ctu_panic("%s (rc=%d)", ers, rc);
+        panic("%s (rc=%d)", ers, rc);
     }
 
     // Get my communicator's size and rank.
@@ -246,7 +257,7 @@ main(
     );
     if (rc != MPI_SUCCESS) {
         ers = "MPI_Comm_size() failed";
-        ctu_panic("%s (rc=%d)", ers, rc);
+        panic("%s (rc=%d)", ers, rc);
     }
 
     int comm_rank;
@@ -256,7 +267,7 @@ main(
     );
     if (rc != MPI_SUCCESS) {
         ers = "MPI_Comm_rank() failed";
-        ctu_panic("%s (rc=%d)", ers, rc);
+        panic("%s (rc=%d)", ers, rc);
     }
 
     // Get base scope.
@@ -269,7 +280,7 @@ main(
     );
     if (rc != QV_SUCCESS) {
         ers = "qv_mpi_scope() failed";
-        ctu_panic("%s (rc=%s)", ers, qv_strerr(rc));
+        panic("%s (rc=%s)", ers, qv_strerr(rc));
     }
 
     print_resources(comm_rank, "Base scope",
@@ -290,14 +301,14 @@ main(
     );
     if (rc != QV_SUCCESS) {
         ers = "qv_split() failed";
-        ctu_panic("%s (rc=%s)", ers, qv_strerr(rc));
+        panic("%s (rc=%s)", ers, qv_strerr(rc));
     }
 
     /* Push into my sub_scope */
     rc = qv_bind_push(sub_scope);
     if (rc != QV_SUCCESS) {
         ers = "qv_bind_push() failed";
-        ctu_panic("%s (rc=%s)", ers, qv_strerr(rc));
+        panic("%s (rc=%s)", ers, qv_strerr(rc));
     }
 
     print_resources(comm_rank, "Phase 1: Regular split:",
@@ -312,13 +323,11 @@ main(
     );
     if (rc != QV_SUCCESS) {
         ers = "qv_hw_obj_count() failed";
-        ctu_panic("%s (rc=%s)", ers, qv_strerr(rc));
+        panic("%s (rc=%s)", ers, qv_strerr(rc));
     }
 
     if (comm_rank == 0)
-        ctu_logf(
-            "\n# Pthread launch on sub_scope(s)\n"
-        );
+        printf("\n# Pthread launch on sub_scope(s)\n");
 
     // Launch Pthreads on respective sub_scope resources.
     do_pthread_things(sub_scope, " sub_scope", comm_rank);
@@ -327,18 +336,14 @@ main(
     rc = qv_barrier(base_scope);
     if (rc != QV_SUCCESS) {
         ers = "qv_barrier() failed";
-        ctu_panic("%s (rc=%s)", ers, qv_strerr(rc));
+        panic("%s (rc=%s)", ers, qv_strerr(rc));
     }
 
     // E.g., launch one kernel per GPU, if GPUs are available.
     if (comm_rank == 0)
-        ctu_logf(
-            "\n# GPU launch on sub_scope(s)\n"
-        );
+        printf("\n# GPU launch on sub_scope(s)\n");
 
-    ctu_str_t *gpustr = ctu_str_new();
-    ctu_str_appendf(
-        gpustr,
+    printf(
         "[%d] %s: launching %d GPU kernel(s)%s\n",
         comm_rank, " sub_scope",
         ngpus, !!ngpus ? " on:" : "."
@@ -350,9 +355,7 @@ main(
             sub_scope, QV_HW_OBJ_GPU, i,
             QV_DEVICE_ID_PCI_BUS_ID, &gpu
         );
-        ctu_str_appendf(
-            gpustr, "[%d]->PCI Bus ID = %s\n", comm_rank, gpu
-        );
+        printf("[%d]->PCI Bus ID = %s\n", comm_rank, gpu);
         // Here are examples on how a user might
         // target the GPUs they got in a QV scope.
         //cudaDeviceGetByPCIBusId(&dev, gpu);
@@ -361,15 +364,12 @@ main(
         free(gpu);
     }
 
-    ctu_emit(base_scope, CTU_SCOPE_KIND_MPI, "%s", ctu_str_cstr(gpustr));
-    ctu_str_del(gpustr);
-
     // Pop back up to the base scope
     // Todo: Do we need to pass a scope to the pop operation?
     rc = qv_bind_pop(sub_scope);
     if (rc != QV_SUCCESS) {
         ers = "qv_bind_pop() failed";
-        ctu_panic("%s (rc=%s)", ers, qv_strerr(rc));
+        panic("%s (rc=%s)", ers, qv_strerr(rc));
     }
 
 
@@ -401,14 +401,14 @@ main(
     );
     if (rc != QV_SUCCESS) {
         ers = "qv_split_at() failed";
-        ctu_panic("%s (rc=%s)", ers, qv_strerr(rc));
+        panic("%s (rc=%s)", ers, qv_strerr(rc));
     }
 
     /* Push into my sub_scope */
     rc = qv_bind_push(numa_scope);
     if (rc != QV_SUCCESS) {
         ers = "qv_bind_push() failed";
-        ctu_panic("%s (rc=%s)", ers, qv_strerr(rc));
+        panic("%s (rc=%s)", ers, qv_strerr(rc));
     }
 
     // Todo: Need to add ordinal color and SPLIT_USE_ALL
@@ -423,7 +423,7 @@ main(
     );
     if (rc != QV_SUCCESS) {
         ers = "qv_group_rank() failed";
-        ctu_panic("%s (rc=%s)", ers, qv_strerr(rc));
+        panic("%s (rc=%s)", ers, qv_strerr(rc));
     }
 
     // How many NUMAs did I get
@@ -435,30 +435,29 @@ main(
     );
     if (rc != QV_SUCCESS) {
         ers = "qv_hw_obj_count() failed";
-        ctu_panic("%s (rc=%s)", ers, qv_strerr(rc));
+        panic("%s (rc=%s)", ers, qv_strerr(rc));
     }
 
     // Barrier not needed; used for tidy output
     rc = qv_barrier(base_scope);
     if (rc != QV_SUCCESS) {
         ers = "qv_barrier() failed";
-        ctu_panic("%s (rc=%s)", ers, qv_strerr(rc));
+        panic("%s (rc=%s)", ers, qv_strerr(rc));
     }
 
-    // Todo: What's the difference between ctu_emit and ctu_logf?
     if (my_numa_rank == 0) {
-        ctu_logf("[%d]->NUMA ID %d: Launching OMP region\n",
+        printf("[%d]->NUMA ID %d: Launching OMP region\n",
                  comm_rank, my_numa_rank);
         do_omp_things(numa_scope, "numa_scope", comm_rank);
     } else {
-        ctu_logf("[%d]->NUMA ID %d\n", comm_rank, my_numa_rank);
+        printf("[%d]->NUMA ID %d\n", comm_rank, my_numa_rank);
     }
 
     // Leader works; everybody else waits
     rc = qv_barrier(numa_scope);
     if (rc != QV_SUCCESS) {
         ers = "qv_barrier() failed";
-        ctu_panic("%s (rc=%s)", ers, qv_strerr(rc));
+        panic("%s (rc=%s)", ers, qv_strerr(rc));
     }
 
     // Pop back up to the base scope
@@ -466,7 +465,7 @@ main(
     rc = qv_bind_pop(numa_scope);
     if (rc != QV_SUCCESS) {
         ers = "qv_bind_pop() failed";
-        ctu_panic("%s (rc=%s)", ers, qv_strerr(rc));
+        panic("%s (rc=%s)", ers, qv_strerr(rc));
     }
 
 
@@ -483,12 +482,12 @@ main(
     );
     if (rc != QV_SUCCESS) {
         ers = "qv_hw_obj_count() failed";
-        ctu_panic("%s (rc=%s)", ers, qv_strerr(rc));
+        panic("%s (rc=%s)", ers, qv_strerr(rc));
     }
 
     if (ngpus == 0) {
         if (comm_rank == 0)
-            ctu_logf("->Skipping: No GPUs found!\n");
+            printf("->Skipping: No GPUs found!\n");
         goto done;
     }
 
@@ -528,19 +527,19 @@ done:
     rc = qv_free(numa_scope);
     if (rc != QV_SUCCESS) {
         ers = "qv_free() failed";
-        ctu_panic("%s (rc=%s)", ers, qv_strerr(rc));
+        panic("%s (rc=%s)", ers, qv_strerr(rc));
     }
 
     rc = qv_free(sub_scope);
     if (rc != QV_SUCCESS) {
         ers = "qv_free() failed";
-        ctu_panic("%s (rc=%s)", ers, qv_strerr(rc));
+        panic("%s (rc=%s)", ers, qv_strerr(rc));
     }
 
     rc = qv_free(base_scope);
     if (rc != QV_SUCCESS) {
         ers = "qv_free() failed";
-        ctu_panic("%s (rc=%s)", ers, qv_strerr(rc));
+        panic("%s (rc=%s)", ers, qv_strerr(rc));
     }
 
     MPI_Finalize();
