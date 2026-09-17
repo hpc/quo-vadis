cd5cac469cd33d45ebbd684c151a0177850746b5
Samuel K. Gutierrez <samuel@lanl.gov>
2026-09-17 12:37:46 -0600

    Cleanup some cmake code.
    Signed-off-by: Samuel K. Gutierrez <samuel@lanl.gov>

--- cmake/QVCheckIncludes.cmake
+++ cmake/QVCheckIncludes.cmake
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2020-2022 Triad National Security, LLC
+# Copyright (c) 2020-2026 Triad National Security, LLC
 #                         All rights reserved.
 #
 # Copyright (c) 2020-2021 Lawrence Livermore National Security, LLC
@@ -12,24 +12,24 @@
 # For autotools-like HAVE_FOO_H (but not as convenient)
 include(CheckIncludeFiles)
 
-# Header checks
-check_include_files(errno.h HAVE_ERRNO_H)
-check_include_files(inttypes.h HAVE_INTTYPES_H)
-check_include_files(limits.h HAVE_LIMITS_H)
-check_include_files(math.h HAVE_MATH_H)
-check_include_files(sched.h HAVE_SCHED_H)
-check_include_files(stdarg.h HAVE_STDARG_H)
-check_include_files(stdbool.h HAVE_STDBOOL_H)
-check_include_files(stdint.h HAVE_STDINT_H)
-check_include_files(stdio.h HAVE_STDIO_H)
-check_include_files(stdlib.h HAVE_STDLIB_H)
-check_include_files(string.h HAVE_STRING_H)
-check_include_files(strings.h HAVE_STRINGS_H)
-check_include_files(sys/resource.h HAVE_SYS_RESOURCE_H)
-check_include_files(sys/stat.h HAVE_SYS_STAT_H)
-check_include_files(sys/time.h HAVE_SYS_TIME_H)
-check_include_files(sys/types.h HAVE_SYS_TYPES_H)
-check_include_files(time.h HAVE_TIME_H)
-check_include_files(unistd.h HAVE_UNISTD_H)
+# Header checks (Note: not used right now).
+#check_include_files(errno.h HAVE_ERRNO_H)
+#check_include_files(inttypes.h HAVE_INTTYPES_H)
+#check_include_files(limits.h HAVE_LIMITS_H)
+#check_include_files(math.h HAVE_MATH_H)
+#check_include_files(sched.h HAVE_SCHED_H)
+#check_include_files(stdarg.h HAVE_STDARG_H)
+#check_include_files(stdbool.h HAVE_STDBOOL_H)
+#check_include_files(stdint.h HAVE_STDINT_H)
+#check_include_files(stdio.h HAVE_STDIO_H)
+#check_include_files(stdlib.h HAVE_STDLIB_H)
+#check_include_files(string.h HAVE_STRING_H)
+#check_include_files(strings.h HAVE_STRINGS_H)
+#check_include_files(sys/resource.h HAVE_SYS_RESOURCE_H)
+#check_include_files(sys/stat.h HAVE_SYS_STAT_H)
+#check_include_files(sys/time.h HAVE_SYS_TIME_H)
+#check_include_files(sys/types.h HAVE_SYS_TYPES_H)
+#check_include_files(time.h HAVE_TIME_H)
+#check_include_files(unistd.h HAVE_UNISTD_H)
 
 # vim: ts=4 sts=4 sw=4 expandtab
