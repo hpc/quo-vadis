83b7b5ef5a44f77342c5702fac1214c148264679
Samuel K. Gutiérrez <samuel@lanl.gov>
2026-09-17 12:43:37 -0600

    Cleanup some cmake code. (#631)
    Signed-off-by: Samuel K. Gutierrez <samuel@lanl.gov>

--- cmake/QVCheckCacheLineSize.cmake
+++ cmake/QVCheckCacheLineSize.cmake
@@ -21,44 +21,46 @@ set(QVI_L1_CACHE_LINESIZE 64 CACHE STRING
 message(CHECK_START "Determining L1 data cache line size")
 
 if(NOT CMAKE_CROSSCOMPILING)
-    set(_qvi_l1_source "default (detection failed)")
-    set(_qvi_l1_probe "${CMAKE_CURRENT_BINARY_DIR}/qvi_l1_cache_linesize.c")
-    file(WRITE "${_qvi_l1_probe}" "
-#ifndef _GNU_SOURCE
-#define _GNU_SOURCE
-#endif
-#include <unistd.h>
-#include <stdio.h>
-int main(void) {
-    long sz = sysconf(_SC_LEVEL1_DCACHE_LINESIZE);
-    /* Signal failure so we fall back to the default. */
-    if (sz <= 0) return 1;
-    printf(\"%ld\", sz);
-    return 0;
-}
-")
-    try_run(
-        _qvi_l1_run_result _qvi_l1_compile_result
-        "${CMAKE_CURRENT_BINARY_DIR}/qvi_l1_cache_linesize"
-        "${_qvi_l1_probe}"
-        RUN_OUTPUT_VARIABLE _qvi_l1_run_output
+    set(qvi_l1_source "default (detection failed)")
+    set(qvi_l1_probe "${CMAKE_CURRENT_BINARY_DIR}/qvi_l1_cache_linesize.c")
+    file(WRITE "${qvi_l1_probe}"
+         "
+             #ifndef _GNU_SOURCE
+             #define _GNU_SOURCE
+             #endif
+             #include <unistd.h>
+             #include <stdio.h>
+             int main(void) {
+                 long sz = sysconf(_SC_LEVEL1_DCACHE_LINESIZE);
+                 /* Signal failure so we fall back to the default. */
+                 if (sz <= 0) return 1;
+                 printf(\"%ld\", sz);
+                 return 0;
+             }
+         "
     )
-    if(_qvi_l1_compile_result AND (_qvi_l1_run_result EQUAL 0)
-       AND (_qvi_l1_run_output MATCHES "^[1-9][0-9]*$"))
-        set(QVI_L1_CACHE_LINESIZE ${_qvi_l1_run_output} CACHE STRING
+    try_run(
+        qvi_l1_run_result qvi_l1_compile_result
+        "${CMAKE_CURRENT_BINARY_DIR}/qvi_l1_cache_linesize"
+        "${qvi_l1_probe}"
+        RUN_OUTPUT_VARIABLE qvi_l1_run_output
+    )
+    if(qvi_l1_compile_result AND (qvi_l1_run_result EQUAL 0)
+       AND (qvi_l1_run_output MATCHES "^[1-9][0-9]*$"))
+        set(QVI_L1_CACHE_LINESIZE ${qvi_l1_run_output} CACHE STRING
             "L1 data cache line size in bytes" FORCE
         )
-        set(_qvi_l1_source "detected")
+        set(qvi_l1_source "detected")
     endif()
-    unset(_qvi_l1_probe)
-    unset(_qvi_l1_run_result)
-    unset(_qvi_l1_compile_result)
-    unset(_qvi_l1_run_output)
+    unset(qvi_l1_probe)
+    unset(qvi_l1_run_result)
+    unset(qvi_l1_compile_result)
+    unset(qvi_l1_run_output)
 else()
-    set(_qvi_l1_source "default (cross-compiling)")
+    set(qvi_l1_source "default (cross-compiling)")
 endif()
 
-message(CHECK_PASS "${QVI_L1_CACHE_LINESIZE} bytes (${_qvi_l1_source})")
-unset(_qvi_l1_source)
+message(CHECK_PASS "${QVI_L1_CACHE_LINESIZE} bytes (${qvi_l1_source})")
+unset(qvi_l1_source)
 
 # vim: ts=4 sts=4 sw=4 expandtab
